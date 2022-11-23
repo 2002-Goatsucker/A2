743cb2486a9e004719aa41db0a0b79ef2fdf7e43
LinJie <1249706509@qq.com>
2022-11-23 14:33:07 +0800

    checkout

--- ChessClient/src/main/java/com/example/chessclient/controller/GameController.java
+++ ChessClient/src/main/java/com/example/chessclient/controller/GameController.java
@@ -11,7 +11,6 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-
 import javafx.scene.shape.Rectangle;
 import java.io.IOException;
 import java.net.URL;
